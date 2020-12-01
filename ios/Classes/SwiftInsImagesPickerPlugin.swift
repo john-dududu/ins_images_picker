3fd4c29a1678d98f6d4fcdba1fbaed651167b074
lecicdjuro <lecicdjuro@gmail.com>
2020-12-01 09:18:14 +0100

    test

--- ios/Classes/SwiftInsImagesPickerPlugin.swift
+++ ios/Classes/SwiftInsImagesPickerPlugin.swift
@@ -47,11 +47,12 @@ public class SwiftInsImagesPickerPlugin: NSObject, FlutterPlugin {
                 config.showsVideoTrimmer = showTrim
                 config.library.mediaType = .video
                 config.video.libraryTimeLimit = videoMaxDuration
-                if #available(iOS 9.0, *) {
-                    config.video.compression = AVAssetExportPresetHEVC1920x1080WithAlpha
-                } else {
-                   config.video.compression =  AVAssetExportPresetMediumQuality
-                }
+
+                if #available(iOS 13.0, *) {
+                     config.video.compression = AVAssetExportPresetHEVC1920x1080WithAlpha
+                    } else {
+                     config.video.compression =  AVAssetExportPresetMediumQuality
+                    }
             }
             config.startOnScreen = .library
             config.library.isSquareByDefault = false
